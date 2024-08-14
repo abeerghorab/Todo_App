2487f86ae98a4fc7f14141ceddf295800288a9c0
abeer <beroghorab068@gmail.com>
2024-08-14 15:48:57 +0300

    add-Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/home/home_screen.dart';
 import 'package:todo_app/my_theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseFirestore.instance.disableNetwork(); //firebase offline
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
